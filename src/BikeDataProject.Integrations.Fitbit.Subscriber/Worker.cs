b9a40f6fb7a363db2fa9ed6607b1d336efaa64de
xivk <ben@itinero.tech>
2020-12-28 20:47:33 +0100

    Activated subscription setup.

--- src/BikeDataProject.Integrations.Fitbit.Subscriber/Worker.cs
+++ src/BikeDataProject.Integrations.Fitbit.Subscriber/Worker.cs
@@ -50,9 +50,9 @@ namespace BikeDataProject.Integrations.FitBit.Subscriber
                 _logger.LogDebug("Worker running at: {time}, triggered every {refreshTime}", 
                     DateTimeOffset.Now, refreshTime);
 
-                // await this.SetupSubscriptions(fitbitCredentials, stoppingToken);
-                // if (stoppingToken.IsCancellationRequested) continue;
-                //
+                await this.SetupSubscriptions(fitbitCredentials, stoppingToken);
+                if (stoppingToken.IsCancellationRequested) continue;
+                
                 // await this.SyncDays(fitbitCredentials, stoppingToken);
                 // if (stoppingToken.IsCancellationRequested) continue;
                 
